c5df22c2adf09c8f16eb62967d3b61d0a27efbe9
AshwinSankar77 <aswin771709@gmail.com>
2024-11-27 12:52:51 +0530

    Add files via upload
    day 5

---
+++ ForLoop1.java
@@ -0,0 +1,13 @@
+package Day5;
+
+public class ForLoop1 {
+ 
+	public static void main(String[] args) {
+	for(int i=0;i<=100;i++) 
+		if (i%5==0) {
+			System.out.println("Ashwin");
+		}else 
+			System.out.println(i);
+}
+}
+
