48df09f45cee3a376ec5d37bc087b03a8a5fb8ca
AshwinSankar77 <aswin771709@gmail.com>
2024-12-03 13:19:51 +0530

    Add files via upload
    day8

---
+++ VaaranamAayiram.java
@@ -0,0 +1,14 @@
+package day8;
+
+public class VaaranamAayiram {
+	public static void main(String[] args) {
+		boolean isTrue=true;
+		if(isTrue) {
+			System.out.println("Suriya weds Meghana");
+			}
+		else {
+			System.out.println("Suriya weds Priya");
+		}
+	}
+
+}
