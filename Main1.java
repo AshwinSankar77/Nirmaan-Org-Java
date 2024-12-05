169e3db1ff25ddc03a404968c4bd85316aeef50c
AshwinSankar77 <aswin771709@gmail.com>
2024-12-05 11:32:03 +0530

    Add files via upload
    day 10

---
+++ Main1.java
@@ -0,0 +1,11 @@
+package day10;
+
+public class Main1 {
+	public static void main(String[] args) {
+		Car A =new Car("Ash");
+		A.displayCarDetails();
+		Car N=new Car("Ash",2024);
+		N.displayCarDetails();
+	}
+
+}
